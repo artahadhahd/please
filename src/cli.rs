8a247cac40468e0de1ccb272b9d4740bf22e3531
artahadhahd <artaazadegan1@gmail.com>
2023-11-23 22:39:08 +0100

    correctly add cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-use clap::Parser;
-
-#[derive(Parser)]
-#[command(author = "artahadhahd", about = "build system", long_about = None)]
-pub struct Cli {
-    #[arg(short, long, num_args(0..))]
-    pub paths: Vec<String>,
-}
-
-impl Cli {
-    pub fn get_result(&self) -> Vec<String> {
-        if self.paths.is_empty() {
-            vec![".".into()]
-        } else {
-            self.paths.to_owned()
-=======
 use clap::{Parser, ValueEnum};
 
 #[derive(Parser, Debug)]
@@ -49,7 +32,6 @@ impl Lang {
         match self {
             Self::c => "c",
             _ => "cpp",
->>>>>>> origin/v2
         }
     }
 }
