6514cd3c8a73555f493f14057a5ad29a69127aa3
artahadhahd <artaazadegan1@gmail.com>
2023-11-23 22:38:17 +0100

    update cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 use clap::Parser;
 
 #[derive(Parser)]
@@ -13,6 +14,42 @@ impl Cli {
             vec![".".into()]
         } else {
             self.paths.to_owned()
+=======
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser, Debug)]
+pub struct Cli {
+    pub initial: Option<Command>,
+    #[arg(long)]
+    pub new: Option<String>,
+    #[arg(short, long)]
+    pub language: Option<Lang>,
+}
+
+#[allow(non_camel_case_types)]
+#[derive(ValueEnum, Clone, Debug)]
+pub enum Command {
+    run,
+    build,
+    scripts,
+}
+
+#[allow(non_camel_case_types)]
+#[derive(ValueEnum, Clone, Debug, Default)]
+pub enum Lang {
+    #[default]
+    c,
+    cpp,
+    #[clap(name = "c++")]
+    cxx,
+}
+
+impl Lang {
+    pub fn to_extension<'a>(&self) -> &'a str {
+        match self {
+            Self::c => "c",
+            _ => "cpp",
+>>>>>>> origin/v2
         }
     }
 }
