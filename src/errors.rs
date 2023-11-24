131191bbba4a63a2809399d701af76cee8c165d4
artahadhahd <artaazadegan1@gmail.com>
2023-11-24 09:51:22 +0100

    i don't know

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::{fmt, path::PathBuf};
 use colored::Colorize;
 
 #[derive(Debug)]
@@ -9,6 +9,7 @@ pub enum CompilationError {
     Dependency(String),
     Cloning(String),
     ShellCommand(String),
+    FailedToCreateFile(PathBuf),
     Ext,
 }
 
@@ -22,6 +23,7 @@ impl std::fmt::Display for CompilationError {
             Dependency(dep) => write!(f, "{}: {dep}", "Dependency not found".bold().red()),
             Cloning(dep) => write!(f, "{} '{dep}'", "Failed to clone dependency".red().bold()),
             ShellCommand(command) => write!(f, "{} '{command}'\nMake sure it's installed on your machine", "Failed to run".red().bold()),
+            FailedToCreateFile(file_name) => write!(f, "{} {:?}", "Couldn't create file".red().bold(), file_name),
             Ext => write!(f, "An internal error occured. please try again."),
         }
     }
